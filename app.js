de8a9e32d62fcae2eb613ceaa93621e98e39b81f
ArthurWangCN <legendaryarthur@163.com>
2022-04-01 23:15:24 +0800

    pin静态页面

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
-// app.js
 App({
-    data: {
-        text: "This is page data."
-    },
-    onLaunch(options) {
-        // Do something initial when launch.
-    },
-    onShow(options) {
-        // Do something when show.
-    },
-    onHide() {
-        // Do something when hide.
-    },
-    onError(msg) {
-        console.log(msg)
-    },
-    globalData: 'I am global data'
+  onLaunch: function () {
+
+  }
 })
--- index/index.js
+++ index/index.js
@@ -1,66 +1,18 @@
-// index/index.js
+
 Page({
-
-    /**
-     * 页面的初始数据
-     */
-    data: {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
+  data: {
+    tags: [
+      { id: '1', name: 'aaa'},
+      { id: '2', name: 'aaa'},
+      { id: '3', name: 'aaa'},
+      { id: '4', name: 'aaa'},
+      { id: '5', name: 'aaa'},
+      { id: '11', name: 'aaa'},
+      { id: '21', name: 'aaa'},
+      { id: '31', name: 'aaaaaaaaaaaa'},
+      { id: '41', name: 'aaa'},
+      { id: '51', name: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'},
+      
+    ]
+  },
 })
